7a6e51bf2f969fbeb95d4f900c394d0fc9d851b2
Dabral, Akhil <Akhil.Dabral.ctr@sabre.com>
2023-09-29 23:14:42 +0530

    for renaming int cloud files

---
+++ RenameUtil.py
@@ -0,0 +1,25 @@
+import os
+
+def rename_int_file(folder):
+    """This method renames the INT files present in the folder in the DEV_CRE format.
+
+    Args:
+        folder (folder) : this is the folder path where all the files are kept
+    """
+    # folder = r"C:\Users\SG0701644\OneDrive - Sabre\Files\INTEncryptedFiles\Sep\7-Sep"
+    all_files = os.listdir(folder)
+
+    for file in all_files:
+        # print(file)
+    
+        file_name = file.replace("INT","DEV")
+        
+        old_file_path = os.path.join(folder,file)
+        new_file_path = os.path.join(folder,file_name)
+        os.rename(old_file_path,new_file_path)
+        print(f"The {file} has been renamed to \n {file_name}\n\n")
+        
+
+if __name__ == "__main__":
+    folder = r"C:\Users\SG0701644\OneDrive - Sabre\Files\INTEncryptedFiles\Sep\28_Sep"
+    rename_int_file(folder)
